c730eb7af92e1d45918c096e4e032e41e64fd5a7
leshawn-rice <leshawnrice3@gmail.com>
2021-08-04 14:23:54 -0700

    changed theme around, added typewriter text to about me section

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import profilePhoto from '../img/profile.png';
+import React, { useEffect } from 'react';
 import '../styles/About.css'
 
 const About = ({ handleLink }) => {
@@ -8,12 +7,43 @@ const About = ({ handleLink }) => {
     window.open('mailto:leshawn.rice@outlook.com?subject=From your site');
   }
 
+  useEffect(() => {
+    const txt = 'Software Developer';
+    let i = 0;
+    // Duration in ms
+    let speed = 200;
+    let newText = document.querySelector('.About-Header').innerText + ' ';
+
+    function typeWriter() {
+      if (i < txt.length) {
+        let header = document.querySelector('.About-Header')
+        newText += txt.charAt(i);
+        header.innerText = newText;
+        i++;
+        setTimeout(typeWriter, speed);
+      }
+    }
+
+    const intervalToClear = setInterval(() => {
+      const el = document.querySelector('.About-Header');
+      el.classList.toggle('has-border');
+    }, 750);
+
+    typeWriter();
+
+    return () => { clearInterval(intervalToClear) };
+
+  }, []);
+
   return (
     <div className="About" id="about">
-      <img src={profilePhoto} className="About-Photo" alt="profile" />
+      <div className="About-BG">
+        <h1 className="About-Header">Leshawn Rice. </h1>
+
+      </div>
       <div className="About-Content">
-        <h1 className="About-Header">About Me</h1>
-        <p className="About-Body">Hello! My name is Leshawn Rice, and I'm a Software Developer from the Greater Seattle Area. I primarily use Javascript and Python but picking up a new language or framework isn't a problem for me. I'm comfortable developing on the frontend and the backend, and I enjoy challenging myself to make seamless, efficient, and scalable applications.</p>
+
+        <p className="About-Body">My name is Leshawn Rice, and I'm a Software Developer from the Greater Seattle Area. I primarily use Javascript and Python but picking up a new language or framework isn't a problem for me. I'm comfortable developing on the frontend and the backend, and I enjoy challenging myself to make seamless, efficient, and scalable applications.</p>
         <div className="About-Footer">
           <button className="About-Contact" onClick={handleContact}>Get in touch</button>
           <p className="About-Social"><span onClick={handleLink} className="About-Link" data-url="https://github.com/leshawn-rice">GitHub</span> <span className="About-Seperator"> |</span> <span onClick={handleLink} className="About-Link" data-url="https://linkedin.com/in/leshawn-rice">LinkedIn</span></p>
