af34005beb51a35e3bceb0040abe86dc684549ca
leshawn-rice <leshawnrice3@gmail.com>
2021-10-09 17:03:04 -0700

    updated resume, added resume to footer, added hover color to navbar linkis

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,18 @@ const Footer = ({ handleLink }) => {
   return (
     <div className="Footer">
       <h1>Leshawn Rice</h1>
-      <h1><span onClick={handleLink} data-url="https://github.com/leshawn-rice" className="Footer-Link">Github</span> | <span onClick={handleLink} data-url="https://linkedin.com/in/leshawn-rice" className="Footer-Link">LinkedIn</span></h1>
+      <h1>
+        <span onClick={handleLink} data-url="/Leshawn_Rice_Resume.pdf" className="Footer-Link">
+          Resume
+        </span>
+        &nbsp;|&nbsp;
+        <span onClick={handleLink} data-url="https://github.com/leshawn-rice" className="Footer-Link">
+          Github
+        </span>
+        &nbsp;|&nbsp;
+        <span onClick={handleLink} data-url="https://linkedin.com/in/leshawn-rice" className="Footer-Link">
+          LinkedIn</span>
+      </h1>
     </div>
   )
 }
