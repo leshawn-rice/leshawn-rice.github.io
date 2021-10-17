4bd9f306b3c179885cfe510da1410c820686414b
Leshawn Rice <leshawn.rice@outlook.com>
2021-10-16 21:24:42 -0700

    added skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,13 +1,42 @@
 import React from 'react'
-import '../styles/Skills.css';
+import Seperator from './Seperator';
+import '../styles/Skills.scss';
 
 const Skills = () => {
   return (
-    <div className="Skills">
-      <div className="Skill-Pill">
-        Hello
+    <>
+      <Seperator />
+      <h1 className="Skills-Header">Skills</h1>
+      <div className="Skills">
+        <div className="Skill-Pill">
+          Javascript
+        </div>
+        <div className="Skill-Pill">
+          Python
+        </div>
+        <div className="Skill-Pill">
+          SQL
+        </div>
+        <div className="Skill-Pill">
+          Express
+        </div>
+        <div className="Skill-Pill">
+          Flask
+        </div>
+        <div className="Skill-Pill">
+          Django
+        </div>
+        <div className="Skill-Pill">
+          React
+        </div>
+        <div className="Skill-Pill">
+          React-Native
+        </div>
+        <div className="Skill-Pill">
+          Ansible
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
